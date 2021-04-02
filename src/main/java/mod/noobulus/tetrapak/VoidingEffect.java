9906081b525003f5cf8c172e897b93bf96c26ae9
Noobulus <mattmattiasmatthew@gmail.com>
2021-04-01 20:41:42 -0500

    Finish up Voiding in perhaps the jankiest way possible (thanks Grimmauld once again for the hack to make exp multiplication on Javelins work), add mundane materials, and polish some things for release.

--- src/main/java/mod/noobulus/tetrapak/VoidingEffect.java
+++ src/main/java/mod/noobulus/tetrapak/VoidingEffect.java
@@ -6,9 +6,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.CombatEntry;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.LazyValue;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.entity.living.LootingLevelEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraft.entity.*;
@@ -16,6 +19,7 @@ import net.minecraft.entity.*;
 import javax.annotation.Nullable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import se.mickelus.tetra.effect.ItemEffect;
@@ -25,12 +29,13 @@ import se.mickelus.tetra.items.modular.ModularItem;
 public class VoidingEffect {
     private static final ItemEffect voiding = ItemEffect.get("tetrapak:voiding");
     private static final LazyValue<Method> arrowStackGetter = new LazyValue<>(() -> ObfuscationReflectionHelper.findMethod(AbstractArrowEntity.class, "func_184550_j"));
+    private static DamageSource lastActiveDamageSource = null;
 
     @Nullable
-    private static ItemStack getThrownItemStack(@Nullable Entity e) { // grimmauld magic
+    private static ItemStack getThrownItemStack(@Nullable Entity e) { // some grimm magic to make javelins work
         if (!(e instanceof AbstractArrowEntity))
             return null;
-        Method lookup = ObfuscationReflectionHelper.findMethod(AbstractArrowEntity.class, "func_184550_j");
+        Method lookup = arrowStackGetter.getValue();
         lookup.setAccessible(true);
         Object result;
         try {
@@ -40,46 +45,26 @@ public class VoidingEffect {
         }
         if (!(result instanceof ItemStack))
             return null;
-        return ((ItemStack) result);
+        return (ItemStack) result;
     }
 
     @SubscribeEvent
     public static void onDeath(LivingDropsEvent event) {
-        if (event.getSource().getTrueSource() != null && event.getSource().getTrueSource() instanceof LivingEntity && !(event.getEntity() instanceof PlayerEntity)) {
-            LivingEntity user = (LivingEntity) event.getSource().getTrueSource();
-            ItemStack heldItem = user.getHeldItemMainhand();
-            ItemStack thrownItem = getThrownItemStack(event.getSource().getImmediateSource());
-            if (heldItem.getItem() instanceof ModularItem) {
-                ModularItem heldModularitem = (ModularItem) heldItem.getItem();
-                int level = heldModularitem.getEffectLevel(heldItem, voiding);
-                if (level > 0) {
-                    event.getDrops().clear();
-                }
-                return;
-            }
-            if (thrownItem.getItem() instanceof ModularItem) {
-                ModularItem thrownModularitem = (ModularItem) thrownItem.getItem();
-                int level = thrownModularitem.getEffectLevel(thrownItem, voiding);
-                if (level > 0) {
-                    event.getDrops().clear();
-                }
-            }
+        if (shouldVoidingAffect(event.getSource(), event.getEntity())) {
+            event.getDrops().clear();
         }
     }
 
     @SubscribeEvent
-    public static void onDeath(LivingExperienceDropEvent event) {
-        LivingEntity user = event.getAttackingPlayer();
-        if ((event.getEntity() instanceof PlayerEntity) || user == null) // accidentally killing somebody and voiding their items is uncool
-            return;
-        ItemStack heldItem = user.getHeldItemMainhand();
-        if (!(heldItem.getItem() instanceof ModularItem))
-            return;
+    public static void bufferDamageSourceEvent(LootingLevelEvent event) { // haha event hacks go brrr
+        lastActiveDamageSource = event.getDamageSource();
+    }
 
-        ModularItem heldModularitem = (ModularItem) heldItem.getItem();
-        int level = heldModularitem.getEffectLevel(heldItem, voiding);
-        if (level > 0) {
-            int levelLooting = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, heldItem);
+    @SubscribeEvent
+    public static void onDeath(LivingExperienceDropEvent event) {
+        LivingEntity target = event.getEntityLiving();
+        if (shouldVoidingAffect(lastActiveDamageSource, target)) {
+            int levelLooting = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, event.getAttackingPlayer().getHeldItemMainhand());
             float modifier = 2 + (0.25f * levelLooting); // double exp, then add 25% more of the original exp for each level of looting
             event.setDroppedExperience((int) (event.getDroppedExperience() * modifier));
         }
@@ -101,5 +86,27 @@ public class VoidingEffect {
                 hardnessExp = (0.2f * (hardness * (1 + (0.25f * levelFortune)))); // give exp based on broken block hardness, needs tweaking
             event.setExpToDrop((int) ((event.getExpToDrop() * modifier) + hardnessExp));
         }
+    }
+
+    private static boolean shouldVoidingAffect(@Nullable DamageSource source, Entity target) {
+        if (source == null)
+            return false;
+        if (source.getTrueSource() instanceof LivingEntity && !(target instanceof PlayerEntity)) {
+            LivingEntity user = (LivingEntity) source.getTrueSource();
+            ItemStack heldItem = user.getHeldItemMainhand();
+
+            if (heldItem.getItem() instanceof ModularItem) {
+                ModularItem heldModularitem = (ModularItem) heldItem.getItem();
+                int level = heldModularitem.getEffectLevel(heldItem, voiding);
+                return level > 0;
+            }
+            ItemStack thrownItem = getThrownItemStack(source.getImmediateSource());
+            if (thrownItem != null && thrownItem.getItem() instanceof ModularItem) {
+                ModularItem thrownModularItem = (ModularItem) thrownItem.getItem();
+                int level = thrownModularItem.getEffectLevel(thrownItem, voiding);
+                return level > 0;
+            }
+        }
+        return false;
     }
 }
