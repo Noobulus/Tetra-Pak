2690ebbcd5c465a0887a13a3c1271b073c8ae942
Katte <herokadendoesminecraft@gmail.com>
2021-07-20 00:32:16 -0700

    im tired and a icant see straight please help me

--- src/main/java/mod/noobulus/tetrapak/eidolon/ReapingEffect.java
+++ src/main/java/mod/noobulus/tetrapak/eidolon/ReapingEffect.java
@@ -1,7 +1,6 @@
-/*
 package mod.noobulus.tetrapak.eidolon;
 
-import src.main.java.elucent.eidolon.Registry;
+//import src.main.java.elucent.eidolon.Registry;
 import mod.noobulus.tetrapak.loot.ReapingLootModifier;
 import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.entity.monster.*;
@@ -20,11 +19,13 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 import se.mickelus.tetra.effect.ItemEffect;
 import se.mickelus.tetra.gui.statbar.GuiStatBar;
 import se.mickelus.tetra.gui.statbar.getter.*;
@@ -43,7 +44,7 @@ public class ReapingEffect implements IHoloDescription, ILootModifier<ReapingLoo
                 double modifier = 1 + (getEffectEfficiency(lastActive) * (levelLooting + 2));
             }
             //Entity victim = event.getEntity();
-            ItemEntity drop = new ItemEntity(new ItemStack(Registry.SOUL_SHARD.get()));
+            ItemEntity drop = new ItemEntity(ForgeRegistries.ITEMS.getValue(new ResourceLocation("eidolon","soul_shard")));
             //drop.setDefaultPickupDelay();
             event.getDrops().add(drop);
         }
@@ -54,5 +55,3 @@ public class ReapingEffect implements IHoloDescription, ILootModifier<ReapingLoo
     @Override
     public ItemEffect getEffect() { return ITetraEffect.get("reaping"); }
 }
-
- */
