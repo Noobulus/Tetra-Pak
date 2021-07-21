6c2028086b7447b086414fd46a9985ef7f49018f
Katte <herokadendoesminecraft@gmail.com>
2021-07-20 22:28:51 -0700

    Added item stack math so it drops 2. sadly its always two and doesnt give a fuck what enchants youve got on teh tool

--- src/main/java/mod/noobulus/tetrapak/eidolon/ReapingEffect.java
+++ src/main/java/mod/noobulus/tetrapak/eidolon/ReapingEffect.java
@@ -1,19 +1,13 @@
 package mod.noobulus.tetrapak.eidolon;
 
-//import src.main.java.elucent.eidolon.Registry;
 import mod.noobulus.tetrapak.loot.ReapingLootModifier;
-import mod.noobulus.tetrapak.loot.VoidingLootModifier;
-import net.minecraft.entity.CreatureAttribute;
-import net.minecraft.entity.monster.*;
 import mod.noobulus.tetrapak.util.DamageBufferer;
-import net.minecraft.entity.player.PlayerEntity;
 import mod.noobulus.tetrapak.util.tetra_definitions.IHoloDescription;
 import mod.noobulus.tetrapak.util.tetra_definitions.ILootModifier;
 import mod.noobulus.tetrapak.util.tetra_definitions.ITetraEffect;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -21,19 +15,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import se.mickelus.tetra.effect.ItemEffect;
-import se.mickelus.tetra.gui.statbar.GuiStatBar;
-import se.mickelus.tetra.gui.statbar.getter.*;
 
-import javax.annotation.Nullable;
 import java.util.function.Function;
 
 public class ReapingEffect implements IHoloDescription, ILootModifier<ReapingLootModifier> {
@@ -44,15 +30,11 @@ public class ReapingEffect implements IHoloDescription, ILootModifier<ReapingLoo
         if (target.getMobType() == CreatureAttribute.UNDEAD) {
             if(event.getSource().getEntity() instanceof PlayerEntity) {
                 int levelLooting = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.MOB_LOOTING, ((PlayerEntity)event.getSource().getEntity()).getMainHandItem());
-                double modifier = 1 + (getEffectEfficiency(lastActive) * (levelLooting + 2));
+                double modifier = 2 + (getEffectEfficiency(lastActive) * (levelLooting));
+                ItemEntity drop = new ItemEntity(target.level, target.getX(), target.getY(), target.getZ(),
+                        new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("eidolon","soul_shard")), ((int) Math.round(modifier))));
+                event.getDrops().add(drop);
             }
-            //Entity victim = event.getEntity();
-            //ItemStack dropnull = new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("eidolon","soul_shard")));
-            //drop.setDefaultPickupDelay();
-            ItemEntity drop = new ItemEntity(target.level, target.getX(), target.getY(), target.getZ(),
-                    new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("eidolon","soul_shard"))));
-            event.getDrops().add(drop);
-
         }
     }
 
