7ca97741da215e70424d97d9fc2cbb5eb9456437
grimmauld <soeren@benjos.de>
2021-07-21 22:17:21 +0200

    Add AND loot condition

--- src/main/java/mod/noobulus/tetrapak/loot/LootConditions.java
+++ src/main/java/mod/noobulus/tetrapak/loot/LootConditions.java
@@ -1,12 +1,14 @@
 package mod.noobulus.tetrapak.loot;
 
 import mod.noobulus.tetrapak.BuildConfig;
+import net.minecraft.client.renderer.model.multipart.AndCondition;
 import net.minecraft.loot.ILootSerializer;
 import net.minecraft.loot.LootConditionType;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.loot.conditions.LootConditionManager;
 
 public enum LootConditions {
+	AND(BuildConfig.MODID + "and", new AndLootCondition.Serializer()),
 	SHOULD_EFFECT_AFFECT(BuildConfig.MODID + ":should_effect_affect", new ShouldEffectAffect.Serializer());
 
 	public final LootConditionType type;
