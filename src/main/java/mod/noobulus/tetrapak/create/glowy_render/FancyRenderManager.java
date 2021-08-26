d4aff2dbdf205c1a438b651fefbe31990afb69a2
grimmauld <soeren@benjos.de>
2021-08-26 21:31:56 +0200

    simplify stuff

--- src/main/java/mod/noobulus/tetrapak/create/glowy_render/FancyRenderManager.java
+++ src/main/java/mod/noobulus/tetrapak/create/glowy_render/FancyRenderManager.java
@@ -2,14 +2,11 @@ package mod.noobulus.tetrapak.create.glowy_render;
 
 import mod.noobulus.tetrapak.TetraPak;
 import mod.noobulus.tetrapak.util.IEventBusListener;
-import net.minecraft.client.renderer.model.IBakedModel;
-import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
@@ -18,12 +15,9 @@ import se.mickelus.tetra.TetraMod;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Supplier;
 
 public class FancyRenderManager implements IEventBusListener {
-	public static final Set<IBakedModel> ISTER_MODELS = new HashSet<>();
 	public static final ResourceLocation MODULAR_DOUBLE = new ResourceLocation(TetraMod.MOD_ID, "modular_double");
 
 	@OnlyIn(Dist.CLIENT)
@@ -34,8 +28,6 @@ public class FancyRenderManager implements IEventBusListener {
 		ItemStackTileEntityRenderer renderer = new ModularItemRenderer();
 		try {
 			Field ister = Item.class.getDeclaredField("ister");
-			if (ister == null)
-				return;
 			ister.setAccessible(true);
 
 			Field modifiersField = Field.class.getDeclaredField("modifiers");
