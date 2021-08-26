84ff0148ede5095f28a3b6fdf02cd465ab08bd77
grimmauld <soeren@benjos.de>
2021-08-26 22:42:47 +0200

    Added a proof of concept rendering

--- src/main/java/mod/noobulus/tetrapak/create/glowy_render/FancyRenderManager.java
+++ src/main/java/mod/noobulus/tetrapak/create/glowy_render/FancyRenderManager.java
@@ -1,13 +1,18 @@
 package mod.noobulus.tetrapak.create.glowy_render;
 
+import mod.noobulus.tetrapak.BuildConfig;
 import mod.noobulus.tetrapak.TetraPak;
 import mod.noobulus.tetrapak.util.IEventBusListener;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ModelRotation;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,10 +20,15 @@ import se.mickelus.tetra.TetraMod;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class FancyRenderManager implements IEventBusListener {
 	public static final ResourceLocation MODULAR_DOUBLE = new ResourceLocation(TetraMod.MOD_ID, "modular_double");
+	public static final Map<String, IBakedModel> GLOWY_BITS = new HashMap<>();
+	private static final String[] GLOWY_BIT_KEYS = new String[]{"axe_glow", "adze_glow"};
 
 	@OnlyIn(Dist.CLIENT)
 	public void onModelRegistry(ModelRegistryEvent event) {
@@ -40,11 +50,20 @@ public class FancyRenderManager implements IEventBusListener {
 		}
 	}
 
+	@OnlyIn(Dist.CLIENT)
+	public void onModelBake(ModelBakeEvent event) {
+		GLOWY_BITS.clear();
+		ModelLoader loader = event.getModelLoader();
+		Arrays.stream(GLOWY_BIT_KEYS).forEach(k ->
+			GLOWY_BITS.put(k, loader.bake(new ResourceLocation(BuildConfig.MODID, "item/glowing/" + k), ModelRotation.X0_Y0)));
+	}
+
 	@Override
 	public void registerModListeners(IEventBus bus) {
 		IEventBusListener.super.registerModListeners(bus);
 		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
 			bus.addListener(this::onModelRegistry);
+			bus.addListener(this::onModelBake);
 		});
 	}
 }
--- src/main/java/mod/noobulus/tetrapak/create/glowy_render/ModularItemRenderer.java
+++ src/main/java/mod/noobulus/tetrapak/create/glowy_render/ModularItemRenderer.java
@@ -2,12 +2,12 @@ package mod.noobulus.tetrapak.create.glowy_render;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.simibubi.create.foundation.item.render.PartialItemModelRenderer;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
@@ -18,14 +18,23 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 public class ModularItemRenderer extends ItemStackTileEntityRenderer {
+	private static final int MAX_LIGHT = 0xF000F0;
+
 	@Override
 	public void renderByItem(ItemStack stack, ItemCameraTransforms.TransformType transformType, MatrixStack ms, IRenderTypeBuffer buffer, int light, int overlay) {
+		IVertexBuilder ivertexbuilder = ItemRenderer.getFoilBufferDirect(buffer, RenderType.cutout(), true, stack.hasFoil());
+		PartialItemModelRenderer renderer = PartialItemModelRenderer.of(stack, transformType, ms, buffer, overlay);
 		IBakedModel mainModel = Minecraft.getInstance()
 			.getItemRenderer()
 			.getModel(stack, null, null);
 
-		RenderType rendertype = RenderTypeLookup.getRenderType(stack, true);
-		IVertexBuilder ivertexbuilder = ItemRenderer.getFoilBufferDirect(buffer, rendertype, true, stack.hasFoil());
+		ms.pushPose();
+
 		Minecraft.getInstance().getItemRenderer().renderModelLists(mainModel, stack, light, overlay, ms, ivertexbuilder);
+
+		ms.translate(0.5, 0.5, 0.5);
+		renderer.renderGlowing(FancyRenderManager.GLOWY_BITS.get("axe_glow"), MAX_LIGHT);
+
+		ms.popPose();
 	}
 }
