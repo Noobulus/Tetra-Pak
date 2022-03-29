240ced5208f50ffbc47281fdc618069dcd8fc858
Noobulus <mattmattiasmatthew@gmail.com>
2022-03-29 14:00:14 -0500

    i hate code dependencies

--- src/main/java/mod/noobulus/tetrapak/mixin/UtilBoosterMixin.java
+++ src/main/java/mod/noobulus/tetrapak/mixin/UtilBoosterMixin.java
@@ -2,7 +2,6 @@ package mod.noobulus.tetrapak.mixin;
 
 import mod.noobulus.tetrapak.Mods;
 import mod.noobulus.tetrapak.util.classloading.GetSudsParticle;
-import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
@@ -20,12 +19,10 @@ import se.mickelus.tetra.items.modular.impl.toolbelt.booster.UtilBooster;
 
 @Mixin(UtilBooster.class)
 public class UtilBoosterMixin {
-	private static SimpleParticleType sudsParticle = Mods.SUPPLEMENTARIES.isLoaded ? GetSudsParticle.getSudsParticle() : null;
-
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"), method = "Lse/mickelus/tetra/items/modular/impl/toolbelt/booster/UtilBooster;boostPlayer(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/nbt/CompoundTag;I)V", cancellable = true)
 	private static void boostPlayer(Player player, CompoundTag tag, int level, CallbackInfo ci) {
 		if (Mods.SUPPLEMENTARIES.isLoaded && hasBubbler(player)) {
-			((ServerLevel)player.level).sendParticles(sudsParticle, player.getX() - 0.2D + Math.random() * 0.4D, player.getY() + Math.random() * 0.2D, player.getZ() - 0.2D + Math.random() * 0.4D, 8, 0.0D, -0.3D, 0.0D, 0.1D);
+			((ServerLevel)player.level).sendParticles(GetSudsParticle.getSudsParticle(), player.getX() - 0.2D + Math.random() * 0.4D, player.getY() + Math.random() * 0.2D, player.getZ() - 0.2D + Math.random() * 0.4D, 8, 0.0D, -0.3D, 0.0D, 0.1D);
 			ci.cancel(); // don't add regular smoke particles
 		}
 	}
@@ -34,7 +31,7 @@ public class UtilBoosterMixin {
 	private static void boostHorizontal(Player player, CallbackInfo ci) {
 		if (Mods.SUPPLEMENTARIES.isLoaded && hasBubbler(player)) {
 			Vec3 direction = getAbsoluteMotion(-player.xxa, -player.zza, player.getYRot());
-			player.getCommandSenderWorld().addParticle(sudsParticle, player.getX(), player.getY() + (double)player.getBbHeight() * 0.4D, player.getZ(), Math.random() * (0.2D * direction.x + 0.07D) - 0.05D, Math.random() * 0.1D - 0.05D, Math.random() * (0.2D * direction.z + 0.07D) - 0.05D);
+			player.getCommandSenderWorld().addParticle(GetSudsParticle.getSudsParticle(), player.getX(), player.getY() + (double)player.getBbHeight() * 0.4D, player.getZ(), Math.random() * (0.2D * direction.x + 0.07D) - 0.05D, Math.random() * 0.1D - 0.05D, Math.random() * (0.2D * direction.z + 0.07D) - 0.05D);
 			ci.cancel(); // don't add regular smoke particles
 		}
 	}
@@ -42,7 +39,7 @@ public class UtilBoosterMixin {
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"), method = "Lse/mickelus/tetra/items/modular/impl/toolbelt/booster/UtilBooster;boostPlayerCharged(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/nbt/CompoundTag;I)V", cancellable = true)
 	private static void boostPlayerCharged(Player player, CompoundTag tag, int level, CallbackInfo ci) {
 		if (Mods.SUPPLEMENTARIES.isLoaded && hasBubbler(player)) {
-			((ServerLevel)player.level).sendParticles(sudsParticle, player.getX() - 0.2D + Math.random() * 0.4D, player.getY() + Math.random() * 0.2D, player.getZ() - 0.2D + Math.random() * 0.4D, 8, 0.0D, -0.3D, 0.0D, 0.1D);
+			((ServerLevel)player.level).sendParticles(GetSudsParticle.getSudsParticle(), player.getX() - 0.2D + Math.random() * 0.4D, player.getY() + Math.random() * 0.2D, player.getZ() - 0.2D + Math.random() * 0.4D, 8, 0.0D, -0.3D, 0.0D, 0.1D);
 			ci.cancel(); // don't add regular smoke particles
 		}
 	}
