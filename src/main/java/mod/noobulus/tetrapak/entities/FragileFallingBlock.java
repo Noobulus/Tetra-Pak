0afb92cfafe2642fed706f9e36ce7788c481388f
Noobulus <mattmattiasmatthew@gmail.com>
2022-03-31 21:10:09 -0500

    did not fix the actual bug, but managed to confuse three separate modders and slightly improve the check to remove the entity in the process

--- src/main/java/mod/noobulus/tetrapak/entities/FragileFallingBlock.java
+++ src/main/java/mod/noobulus/tetrapak/entities/FragileFallingBlock.java
@@ -3,18 +3,31 @@ package mod.noobulus.tetrapak.entities;
 import mod.noobulus.tetrapak.registries.Entities;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.DirectionalPlaceContext;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.entity.IEntityAdditionalSpawnData;
 import net.minecraftforge.network.NetworkHooks;
@@ -44,7 +57,6 @@ public class FragileFallingBlock extends FallingBlockEntity implements IEntityAd
 		this.yo = pos.getY();
 		this.zo = pos.getZ() + .5;
 		this.setStartPos(blockPosition());
-		this.drops = NonNullList.of(ItemStack.EMPTY, drops.toArray(new ItemStack[0]));
 	}
 
 	@Override
@@ -61,13 +73,15 @@ public class FragileFallingBlock extends FallingBlockEntity implements IEntityAd
 			setDeltaMovement(getDeltaMovement().add(0.0D, -0.04D, 0.0D));
 
 		move(MoverType.SELF, getDeltaMovement());
-		if (!level.isClientSide && (onGround || (time > 100 && (getY() < -63 || getY() > 320)) || this.time > 600))
-			dropItems();
+		if (onGround || (time > 100 && (getY() < -63 || getY() > 320)) || this.time > 600) {
+			this.dropItems();
+			return;
+		}
 		setDeltaMovement(getDeltaMovement().scale(0.98D));
 	}
 
-	private void dropItems() {
-		if (!level.isClientSide)
+	private void dropItems() { // TODO: why the hell does this not sync to the client?
+		if (!level.isClientSide && this.dropItem && this.level.getGameRules().getBoolean(GameRules.RULE_DOENTITYDROPS))
 			drops.forEach(stack -> Block.popResource(level, blockPosition(), stack));
 		this.discard();
 	}
@@ -84,7 +98,6 @@ public class FragileFallingBlock extends FallingBlockEntity implements IEntityAd
 			return;
 		super.readAdditionalSaveData(nbt);
 		drops = NonNullList.withSize(nbt.getList("Items", 10).size(), ItemStack.EMPTY);
-		ContainerHelper.loadAllItems(nbt, drops);
 		ContainerHelper.loadAllItems(nbt, drops);
 	}
 
